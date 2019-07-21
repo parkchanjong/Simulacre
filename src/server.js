4ebe0604f8444d8a2c97f9b7c4433eeeca2988b3
parkchanjong <7000cj@gmail.com>
2019-07-21 18:31:47 +0900

    scret setup

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
-require('dotenv').config();
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+
 import { GraphQLServer } from 'graphql-yoga';
 import logger from 'morgan';
 import schema from './schema';
+import { sendSecretMail } from './utils';
+
+sendSecretMail('7000cj@gmail.com', '1234');
 
 const PORT = process.env.PORT || 4000;
 
@@ -9,4 +15,4 @@ const server = new GraphQLServer({ schema });
 
 server.express.use(logger('dev'));
 
-server.start({ port: PORT }, () => console.log(` Server running on http://localhost:${PORT}`));
+server.start({ port: PORT }, () => console.log(`✅ Server running on http://localhost:${PORT}`));
