b2364636b50779e1bb45d7dcbfb7527b67c748dc
parkchanjong <7000cj@gmail.com>
2019-07-24 17:29:28 +0900

    passport JWT setup

--- src/server.js
+++ src/server.js
@@ -1,18 +1,18 @@
-import dotenv from 'dotenv';
-import path from 'path';
-dotenv.config({ path: path.resolve(__dirname, '.env') });
-
+import './env';
 import { GraphQLServer } from 'graphql-yoga';
 import logger from 'morgan';
 import schema from './schema';
-import { sendSecretMail } from './utils';
-
-sendSecretMail('7000cj@gmail.com', '1234');
+import './passport';
+import { authenticateJwt } from './passport';
 
 const PORT = process.env.PORT || 4000;
 
-const server = new GraphQLServer({ schema });
+const server = new GraphQLServer({
+	schema,
+	context: ({ request }) => ({ request }),
+});
 
 server.express.use(logger('dev'));
+server.express.use(authenticateJwt);
 
 server.start({ port: PORT }, () => console.log(`✅ Server running on http://localhost:${PORT}`));
--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,7 @@
-import dotenv from 'dotenv';
-import path from 'path';
-dotenv.config({ path: path.resolve(__dirname, '.env') });
-
 import { adjectives, nouns } from './words';
 import nodemailer from 'nodemailer';
 import sgTransport from 'nodemailer-sendgrid-transport';
+import jwt from 'jsonwebtoken';
 
 export const generateSecret = () => {
 	const randomNumber = Math.floor(Math.random() * adjectives.length);
@@ -24,10 +21,12 @@ const sendMail = email => {
 
 export const sendSecretMail = (adress, secret) => {
 	const email = {
-		from: 'cj@Simulacre.com',
+		from: 'nico@prismagram.com',
 		to: adress,
-		subject: '🔒Login Secret for Simulacre🔒',
-		html: `Hello! Your login secret it ${secret}.<br/>Copy paste on the app/website to log in`,
+		subject: '🔒Login Secret for Prismagram🔒',
+		html: `Hello! Your login secret is <strong>${secret}</strong>.<br/>Copy paste on the app/website to log in`,
 	};
 	return sendMail(email);
 };
+
+export const generateToken = id => jwt.sign({ id }, process.env.JWT_SECRET);
